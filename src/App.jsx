14fbf0068521fa09cf5bb7160d099a4b6013779e
dwqcwda <lty5582@naver.com>
2024-12-18 16:11:28 +0900

    react day2

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//class App extends Component {
+//  render() {
+//    return (
+//      <div>
+//        <h1>React 컴포넌트에는 두 가지 종류가 있죠.</h1>
+//        <div>
+//          이것은 클래스형 컴포넌트입니다
+//          <p>React 너무나도 재밌죠 !?</p>
+//          <p>JavaScript와는 다른 새로운 세계가 펼쳐집니다</p>
+//          <span>기 대 된 다</span>
+//        </div>
+//      </div>
+//    )
+//  }
+//}
+
+function App() {
+  return (
+    <>
+      <h1>이건 클래스형 컴포넌트입니다</h1>
+      <p>JavaScript의 클래스 문법을 활용하지요!</p>
+    </>
+  )
+}
+
+
+
+export default App
